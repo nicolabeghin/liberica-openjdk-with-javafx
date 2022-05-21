1e565064330f9550256c518aaaf9c234f80d6133
Nicola Beghin <nicolabeghin@gmail.com>
2022-05-21 12:13:11 +0200

    use full jdk otherwise javafx not available

--- build.sh
+++ build.sh
@@ -12,16 +12,19 @@ DOCKERHUB_ACCOUNT=nicolabeghin
 PROJECT=liberica-openjdk-with-javafx-${FLAVOR}
 
 # download original Dockerfile and enable back javafx
-rm -fr Dockerfile* &> /dev/null
+rm -f Dockerfile* &> /dev/null
 echo "Downloading original Dockerfile from ${URL}${FLAVOR}/${JDK_VERSION}/Dockerfile"
-curl -s ${URL}${FLAVOR}/${JDK_VERSION}/Dockerfile | grep -v javafx > Dockerfile
-
-# enable back javafx
-rm Dockerfile-original
+curl -s ${URL}${FLAVOR}/${JDK_VERSION}/Dockerfile | 
+    grep -v javafx |
+    sed 's/ARG LIBERICA_IMAGE_VARIANT.*/ARG LIBERICA_IMAGE_VARIANT=standard/' |
+    sed 's/RSUFFIX=""/RSUFFIX="-full"/'> Dockerfile
 
 # build image
-echo "Building tags $DOCKERHUB_ACCOUNT/$PROJECT:${JDK_VERSION}"
-docker build -t $DOCKERHUB_ACCOUNT/$PROJECT:${JDK_VERSION} .
+echo "Building tags ${DOCKERHUB_ACCOUNT}/${PROJECT}:${JDK_VERSION}"
+docker build --build-arg LIBERICA_IMAGE_VARIANT=standard --build-arg LIBERICA_VM=all -t ${DOCKERHUB_ACCOUNT}/${PROJECT}:${JDK_VERSION} .
 
 # push image to DockerHub
-docker push $DOCKERHUB_ACCOUNT/$PROJECT:${JDK_VERSION}
+docker push ${DOCKERHUB_ACCOUNT}/${PROJECT}:${JDK_VERSION}
+
+# cleanup
+rm -f Dockerfile* &> /dev/null
